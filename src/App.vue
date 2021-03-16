9148352ce130f5f2dd390f1a130b53fc32f984e7
Hanna-Syniavska <annarybalko34@gmail.com>
2021-03-16 18:22:59 +0200

    init app

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app id="app" app dark>
+    <TheMainLayout>
+      <router-view/>
+    </TheMainLayout>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import TheMainLayout from '@/layouts/TheMainLayout'
+export default {
+  
+  name: "App",
+  components: {
+    TheMainLayout
   }
 }
-</style>
+
+</script>
